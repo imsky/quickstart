fc299db2be30e72193c1b26b9f25922c58a78cd2
Ivan Malopinsky <ivan@imsky.co>
2015-05-21 01:34:34 -0400

    basic -> base

--- gulp/base.gulpfile.js
+++ gulp/base.gulpfile.js
@@ -1,27 +1,36 @@
+/*
+Basic Gulpfile from Quickstart
+qkst.io/gulp/basic.js
+*/
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var del = require('del');
 
+var paths = {
+	'src': './',
+	'dist': 'dist/'
+};
+
 var files = {
-	'src': '*.js',
-	'dist': 'dist/*.js'
+	'src': paths.src + '*.js',
+	'dist': paths.dist + '*.js'
 };
 
 gulp.task('clean', function (cb) {
-	del('dist/', cb);
+	del(paths.dist, cb);
 });
 
 gulp.task('scripts', ['clean'], function () {
 	return gulp.src(files.src)
-		.pipe(concat('out.js'))
-        .pipe(gulp.dest('dist/'));	
+		.pipe(concat('bundle.js'))
+        .pipe(gulp.dest(paths.dist));	
 });
 
 gulp.task('minify', ['scripts'], function () {
 	return gulp.src(files.dist)
-		.pipe(uglify('out.min.js'))
-		.pipe(gulp.dest('dist/'));
+		.pipe(uglify('bundle.min.js'))
+		.pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('build', ['minify']);
