d1c529e067ab40ca6274e2dc5bd5cea3fb8ab135
Ivan Malopinsky <ivan@imsky.co>
2015-07-01 22:12:18 -0400

    add download name logic using option-files

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -2,17 +2,18 @@ from bottle import route, request, run, template, error, abort, static_file
 import os, string, pprint
 
 def find_file(type, name):
-  """searches file within directory by name, ignoring extension"""
+  """searches file within directory by name, ignoring extension and case"""
   dir = 'files/' + type
   options = []
+  # todo: sanitize type and name
   if os.path.isdir(dir):
     for root, dirs, files in os.walk(dir):
       for f in files:
         if f[0] == '.':
-          options.append(f)
+          options.append(f[1:])
         else:
-          splitName = string.split(f, '.')
-          filename = splitName[0]
+          split = string.split(f, '.')
+          filename = split[0]
           if filename.lower() == name.lower():
             return [options, dir, f]
   return False
@@ -24,9 +25,18 @@ def render(type, name):
   if lookup:
     options, dir, filename = lookup
     download = False
+    # todo: make dl flag more concise
     if request.query_string == 'download' or request.query_string == 'dl':
-      # todo: use options for download file name
+      split = string.split(filename, '.')
       download = True
+
+      if 'no-dot' in options:
+        # name.ext -> ext
+        download = split[1]
+      elif 'no-name' in options:
+        # name.ext -> .ext
+        download = '.' + split[1]
+
     return static_file(filename, dir, mimetype='text/plain', download=download)
   else:
     abort(404, 'File not found')
