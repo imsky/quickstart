2b344054ca20014d3393d0d6f8acfb7fea39d590
Ivan Malopinsky <ivan@imsky.co>
2015-07-07 22:59:48 -0400

    add gulpfile for babel+webpack

---
+++ files/javascript/gulpfile-babel-webpack.js
@@ -0,0 +1,23 @@
+// npm install --save-dev gulp gulp-webpack babel-loader
+
+var gulp = require('gulp');
+var webpack = require('gulp-webpack');
+
+gulp.task('transpile', function() {
+  return gulp.src('index.js')
+    .pipe(webpack({
+      output: {
+        library: 'Library',
+        filename: 'library.js',
+        libraryTarget: 'umd'
+      },
+      module: {
+        loaders: [{
+          loader: 'babel-loader'
+        }]
+      }
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['transpile']);
