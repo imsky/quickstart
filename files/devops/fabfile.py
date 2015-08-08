7f9cd6b0030b37e8dc1b9be66d322ba2599597bf
Ivan Malopinsky <ivan@imsky.co>
2015-08-08 15:23:59 -0400

    pep8 fabfile

--- files/devops/fabfile.py
+++ files/devops/fabfile.py
@@ -2,9 +2,9 @@
 # qkst.io/devops/fabfile
 
 from fabric.api import (
-  task, parallel, roles,
-  run, local, sudo, put,
-  env, settings
+    task, parallel, roles,
+    run, local, sudo, put,
+    env, settings
 )
 
 from fabric.contrib.project import rsync_project
@@ -14,41 +14,46 @@ from fabric.tasks import execute
 env.user = 'root'
 
 env.roledefs = {
-  'local': ['localhost:22']
+    'local': ['localhost:22']
 }
 
 env.roledefs['all'] = [host for role in env.roledefs.values() for host in role]
 
+
 @task
 @roles('local')
 def setup():
-  sudo('apt-get update')
-  sudo('apt-get install -y python python-pip python-virtualenv')
-  run('pip install fabric')
+    sudo('apt-get update')
+    sudo('apt-get install -y python python-pip python-virtualenv')
+    run('pip install fabric')
+
 
 @task
 @parallel
 def install_deb(url):
-  sudo('wget %s -O /tmp/download.deb' % url)
-  sudo('dpkg -i /tmp/download.deb && rm /tmp/download.deb')
+    sudo('wget %s -O /tmp/download.deb' % url)
+    sudo('dpkg -i /tmp/download.deb && rm /tmp/download.deb')
+
 
 @task
 def upload(local='./', remote='/tmp'):
-  rsync_project(
-    local_dir=local,
-    remote_dir=remote,
-    exclude=['.git'],
-    extra_opts='-lp'  # preserve symlinks and permissions
-  )
+    rsync_project(
+        local_dir=local,
+        remote_dir=remote,
+        exclude=['.git'],
+        extra_opts='-lp'  # preserve symlinks and permissions
+    )
+
 
 @task
 def put_as_user(file, remote, user):
-  with settings(user=user):
-    put(file, remote)
+    with settings(user=user):
+        put(file, remote)
+
 
 @task
 def context_demo():
-  with cd('/tmp'):
-    run('touch testfile')
-  with prefix('cd /tmp')
-    run('rm testfile')
+    with cd('/tmp'):
+        run('touch testfile')
+    with prefix('cd /tmp')
+        run('rm testfile')
